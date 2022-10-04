1d2eee636b86b2550b4be9e0452505567f8edf98
martin-creator <martinlubowa@outlook.com>
2022-10-04 19:29:56 +0300

    Successfully added bootstrap to django project

--- core/views.py
+++ core/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def customer_page(request):
+    return render(request, 'home.html')
+
+def courier_page(request):
+    return render(request, 'home.html')
