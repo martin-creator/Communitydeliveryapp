fa417f0804b7e55733c53597acf61c04cf47aa5c
martin-creator <martinlubowa@outlook.com>
2022-10-12 17:29:38 +0300

    Created customer base template

--- core/customer/views.py
+++ core/customer/views.py
@@ -8,4 +8,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required()
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'customer/base.html')
