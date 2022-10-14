003122c4d0aee4c14be77e680f3b20be2760b001
martin-creator <martinlubowa@outlook.com>
2022-10-14 12:41:41 +0300

    Added logic to change passwords

--- core/customer/views.py
+++ core/customer/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from core.customer import forms
 
 from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 # Create your views here.
 
@@ -12,23 +14,37 @@ from django.contrib import messages
 def home(request):
     return redirect(reverse('customer:profile'))
 
+
 @login_required(login_url="/sign-in/?next=/customer/")
 def profile_page(request):
     user_form = forms.BasicUserForm(instance=request.user)
     customer_form = forms.BasicCustomerForm(instance=request.user.customer)
+    password_form = PasswordChangeForm(request.user)
 
     if request.method == "POST":
-        user_form = forms.BasicUserForm(request.POST, instance=request.user)
-        customer_form = forms.BasicCustomerForm(request.POST, request.FILES, instance=request.user.customer)
-        if user_form.is_valid() and customer_form.is_valid():
-            customer_form.save()
-            user_form.save()
+        if request.POST.get('action') == 'update_profile':
+            user_form = forms.BasicUserForm(
+                request.POST, instance=request.user)
+            customer_form = forms.BasicCustomerForm(
+                request.POST, request.FILES, instance=request.user.customer)
+            if user_form.is_valid() and customer_form.is_valid():
+                customer_form.save()
+                user_form.save()
 
-            messages.success(request, 'Your profile has been updated')
-            return redirect(reverse('customer:profile'))
+                messages.success(request, 'Your profile has been updated')
+                return redirect(reverse('customer:profile'))
+
+        elif request.POST.get('action') == 'update_password':
+            password_form = PasswordChangeForm(request.user, request.POST)
+            if password_form.is_valid():
+                user = password_form.save()
+                update_session_auth_hash(request, user)
+
+                messages.success(request, 'Your password has been updated')
+                return redirect(reverse('customer:profile'))
 
     return render(request, 'customer/profile.html', {
         "user_form": user_form,
-        "customer_form": customer_form
+        "customer_form": customer_form,
+        "password_form": password_form
     })
-
