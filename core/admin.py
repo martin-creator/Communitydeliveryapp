266beb543ddfe3efe445a0bf96f972e8742b9ddb
martin-creator <martinlubowa@outlook.com>
2022-10-18 12:26:59 +0300

    Created models for job and catergory

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from . import models
+#from . import models
+from .models import *
 # Register your models here.
-admin.site.register(models.Customer)
+admin.site.register(Customer)
+admin.site.register(Category)
+admin.site.register(Job)
--- core/models.py
+++ core/models.py
@@ -1,15 +1,70 @@
 from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
+from django.utils import timezone
 # Create your models here.
+
+
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='customer/avatars/', blank=True, null=True)
+    avatar = models.ImageField(
+        upload_to='customer/avatars/', blank=True, null=True)
     phone_number = models.CharField(max_length=50, blank=True)
     stripe_customer_id = models.CharField(max_length=255, blank=True)
     stripe_payment_method_id = models.CharField(max_length=255, blank=True)
     stripe_card_last4 = models.CharField(max_length=255, blank=True)
 
-
-    def __str__(self) :
+    def __str__(self):
         return self.user.get_full_name()
+
+
+class Category(models.Model):
+    slug = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    SMALL_SIZE = "small"
+    MEDIUM_SIZE = "medium"
+    LARGE_SIZE = "large"
+    SIZES = (
+        (SMALL_SIZE, 'Small'),
+        (MEDIUM_SIZE, 'Medium'),
+        (LARGE_SIZE, 'Large'),
+    )
+
+    CREATING_STATUS = "creating"
+    PROCESSING_STATUS = "processing"
+    PICKING_STATUS = "picking"
+    DELIVERING_STATUS = "delierving"
+    COMPLETED_STATUS = "completed"
+    CANCELED_STATUS = "canceled"
+    STATUSES = (
+        (CREATING_STATUS, 'Creating'),
+        (PROCESSING_STATUS, 'Processing'),
+        (PICKING_STATUS, 'Picking'),
+        (DELIVERING_STATUS, 'Delivering'),
+        (COMPLETED_STATUS, 'Completed'),
+        (CANCELED_STATUS, 'Canceled')
+    )
+
+    # Step 1
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    size = models.CharField(max_length=20, choices=SIZES, default=MEDIUM_SIZE)
+    quantity = models.IntegerField(default=1)
+    photo = models.ImageField(upload_to='job/photos')
+    status = models.CharField(
+        max_length=20, choices=STATUSES, default=CREATING_STATUS)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
