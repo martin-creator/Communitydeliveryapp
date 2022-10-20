413f4e32da20d11c051c775be0fe6db4bd6523bb
martin-creator <martinlubowa@outlook.com>
2022-10-20 15:48:18 +0300

    Created courier job details page

--- core/courier/views.py
+++ core/courier/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.conf import settings
-
+from core.models import *
 
 
 # Create your views here.
@@ -17,4 +17,33 @@ def home(request):
 def available_jobs_page(request):
     return render(request, 'courier/available_jobs.html', {
         "GOOGLE_MAP_API_KEY": settings.GOOGLE_MAP_API_KEY,
+    })
+
+@login_required(login_url="/sign-in/?next=/courier/")
+def available_job_page(request, id): 
+    job = Job.objects.filter(id=id, status = Job.PROCESSING_STATUS).last()
+
+    if not job:
+        return redirect(reverse('courier:available_jobs'))
+
+    # if request.method == "POST":
+    #     job.courier = request.user.courier
+    #     job.status = Job.PICKING_STATUS
+    #     job.save()
+
+    #     try:
+    #         layer = get_channel_layer()
+    #         async_to_sync(layer.group_send)("job_" + str(job.id), {
+    #             'type': 'job_update',
+    #             'job': {
+    #                 'status': job.get_status_display(),
+    #             }
+    #         })
+    #     except:
+    #         pass
+
+    #     return redirect(reverse('courier:available_jobs'))
+
+    return render(request, 'courier/available_job.html', {
+        "job": job,
     })
