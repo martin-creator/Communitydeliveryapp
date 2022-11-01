5d4fd5769cd2760f8427e7794c101a4b180c5255
martin-creator <martinlubowa@outlook.com>
2022-11-01 15:53:09 +0300

    Added customer location in realtime

--- core/courier/apis.py
+++ core/courier/apis.py
@@ -1,3 +1,6 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
@@ -34,17 +37,17 @@ def current_job_update_api(request, id):
         job.status = Job.DELIVERING_STATUS
         job.save()
 
-        # try:
-        #     layer = get_channel_layer()
-        #     async_to_sync(layer.group_send)("job_" + str(job.id), {
-        #         'type': 'job_update',
-        #         'job': {
-        #             'status': job.get_status_display(),
-        #             'pickup_photo': job.pickup_photo.url,
-        #         }
-        #     })
-        # except:
-        #     pass
+        try:
+            layer = get_channel_layer()
+            async_to_sync(layer.group_send)("job_" + str(job.id), {
+                'type': 'job_update',
+                'job': {
+                    'status': job.get_status_display(),
+                    'pickup_photo': job.pickup_photo.url,
+                }
+            })
+        except:
+            pass
 
     elif job.status == Job.DELIVERING_STATUS:
         job.delivery_photo = request.FILES['delivery_photo']
@@ -52,17 +55,17 @@ def current_job_update_api(request, id):
         job.status = Job.COMPLETED_STATUS
         job.save()
 
-        # try:
-        #     layer = get_channel_layer()
-        #     async_to_sync(layer.group_send)("job_" + str(job.id), {
-        #         'type': 'job_update',
-        #         'job': {
-        #             'status': job.get_status_display(),
-        #             'delivery_photo': job.delivery_photo.url,
-        #         }
-        #     })
-        # except:
-        #     pass
+        try:
+            layer = get_channel_layer()
+            async_to_sync(layer.group_send)("job_" + str(job.id), {
+                'type': 'job_update',
+                'job': {
+                    'status': job.get_status_display(),
+                    'delivery_photo': job.delivery_photo.url,
+                }
+            })
+        except:
+            pass
 
     return JsonResponse({
         "success": True
